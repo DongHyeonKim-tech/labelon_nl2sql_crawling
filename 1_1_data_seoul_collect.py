5e9cfcd308292f22d1fa16744b82173ab15f75d9
gchshin <gcshin@euclidsoft.co.kr>
2022-06-08 17:27:54 +0900

    data.seoul.go.kr에서 openAPI 크롤링 부분 수정

--- 1_1_data_seoul_collect.py
+++ 1_1_data_seoul_collect.py
@@ -12,8 +12,8 @@ from tqdm import tqdm
 from bs4 import BeautifulSoup
 
 # from requests_html import HTMLSession
-
 # 서울 열린데이터 광장 데이터 수집 1차
+# 리스트 수집
 
 """
 def render_JS(URL):c
@@ -36,10 +36,10 @@ if __name__=="__main__":
     )
     cur = conn.cursor()
 
+    print('===== Tibero DB Test ====================================================================')
     sql = "SELECT * FROM DATA_BASIC_INFO"
     cur.execute(sql)
     print(cur.fetchall())
-
     print('=========================================================================================')
     
     param_list = []
@@ -49,7 +49,7 @@ if __name__=="__main__":
     search_url = basic_url + "datasetList.do"
 
     # post 데이터
-    post_data = {"pageIndex1": "1", "sortColBy1": "R"}
+    post_data = {"pageIndex": "1", "sortColBy": "R", "datasetKind": "1"}
 
     response = requests.post(f'{search_url}', post_data)
 
@@ -61,23 +61,29 @@ if __name__=="__main__":
     
     temp = search_result % 10
     total_index = int(search_result / 10) + (0 if search_result % 10 == 0 else 1)
+    
+    print("search_result = " + str(search_result) + ", total_index = " + str(total_index))
 
     # 일부 데이터로 테스트
-    total_index = 3
+    # total_index = 3
     
     # 저장 데이셋
     df_data = pd.DataFrame(columns=['title', 'data_rel', 'data_type', 'make_url'])
 
-
+    # Sleep 시간 랜덤
+    list_random = [5.02, 5.51, 4.93, 5.35, 5.17, 4.81, 5.77, 5.23, 4.56, 5.49]
+    len_random = len(list_random)
+    
     # 리스트에 있는 데이터셋 기본 항목 저장
     for i in tqdm(range(0, total_index)):
 
-        save_index = i * 10
+        i_sleep = i % len_random
+        time.sleep(list_random[i_sleep])
 
-        time.sleep(5)
+        post_index_data = {"pageIndex": i+1, "sortColBy": "R", "datasetKind": "1"}
 
-        post_index_data = {"pageIndex1": i+1, "sortColBy1": "R"}
-
+        print(post_index_data)
+        
         response_index = requests.post(f'{search_url}', post_index_data)
         html_index = response_index.text
 
@@ -89,8 +95,6 @@ if __name__=="__main__":
             chk_openapi = [0, 0]            # OpenAPI 인 경우
 
             # Insert Data 정의
-            save_index += 1
-
             ts = time.time()
             timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
 
@@ -122,7 +126,7 @@ if __name__=="__main__":
 
             if chk_openapi[0] == 1:
                 # Open API 인 경우
-                print(chk_openapi)
+                # print(chk_openapi)
                 collect_data_type = 'OpenAPI'
                 collect_url = basic_url + provide_list_url[chk_openapi[1]]
             else:
@@ -130,20 +134,19 @@ if __name__=="__main__":
                 collect_data_type = provide_list_data_type[0]
                 collect_url = basic_url + provide_list_url[0]
                 
-                print(chk_openapi)
+                # print(chk_openapi)
             
-
+            # DATA_BASIC_INFO 데이터 입력
             insert_sql = """INSERT INTO DATA_BASIC_INFO (ID, COLLECT_SITE_ID, DATA_NAME, DATA_ORIGIN_KEY, PROVIDE_DATA_TYPE, PROVIDE_URL_LINK, """\
-                         """COLLECT_DATA_TYPE, COLLECT_URL_LINK, IS_COLLECT_YN, SYSTEM_REGISTER_DATE) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"""
+                         """COLLECT_DATA_TYPE, COLLECT_URL_LINK, IS_COLLECT_YN, SYSTEM_REGISTER_DATE) VALUES (SEQ_BASIC.NEXTVAL, ?, ?, ?, ?, ?, ?, ?, ?, ?)"""
 
-            insert_values = (save_index, 1, title_name, data_rel, str(provide_list_data_type), str(provide_list_url), collect_data_type, collect_url, 'N', timestamp)
+            insert_values = (1, title_name, data_rel, str(provide_list_data_type), str(provide_list_url), collect_data_type, collect_url, 'N', timestamp)
             cur.execute(insert_sql, insert_values)
             
             df_data = df_data.append({'title': title_name, 'data_rel': data_rel, 'data_type': provide_list_data_type, 'make_url': provide_list_url}, ignore_index=True)
 
-
-
-
     cur.close()
 
+    print('=========================================================================================')
+    print('===== END ===============================================================================')
     print('=========================================================================================')
--- 1_2_data_seoul_physical.py
+++ 1_2_data_seoul_physical.py
@@ -4,6 +4,7 @@ import re
 import time
 import json
 import pandas as pd
+from pymysql import NULL
 import requests
 import datetime
 import jaydebeapi
@@ -15,8 +16,8 @@ from tqdm import tqdm
 from bs4 import BeautifulSoup
 
 # from requests_html import HTMLSession
-
-# 서울 열린데이터 광장 데이터 수집 1차
+# 서울 열린데이터 광장 데이터 수집 2차
+# 세부 항목 수집 및 OpenAPI 접속 후 데이터 수집
 
 if __name__=="__main__":
 
@@ -33,12 +34,17 @@ if __name__=="__main__":
 
     basic_url = "http://data.seoul.go.kr/dataList/"
 
-    # 인증키
-    # 716a72767867636833306d6a536955
+    # 인증키 : 716a72767867636833306d6a536955
     
     auth_key = '716a72767867636833306d6a536955'
 
-    sql = "SELECT ID, COLLECT_SITE_ID, DATA_NAME, DATA_ORIGIN_KEY, COLLECT_DATA_TYPE, COLLECT_URL_LINK, IS_COLLECT_YN FROM DATA_BASIC_INFO"
+    # 서울 열린데이터 광장 데이터 & 수집 데이터 타입이 OpenAPI 인 경우에 데이터 수집
+    sql = "SELECT ID, COLLECT_SITE_ID, DATA_NAME, DATA_ORIGIN_KEY, COLLECT_DATA_TYPE, COLLECT_URL_LINK, IS_COLLECT_YN "\
+        + "  FROM DATA_BASIC_INFO "\
+        + " WHERE COLLECT_SITE_ID = 1 "\
+        + "   AND COLLECT_DATA_TYPE = 'OpenAPI' "\
+        + "   AND ROWNUM <= 20 "
+
     cur.execute(sql)
     sql_result = cur.fetchall()
 
@@ -86,16 +92,12 @@ if __name__=="__main__":
         master_openapi_url = sample_openapi_url.replace("/sample/", "/" + auth_key + "/")
 
         master_openapi_url = master_openapi_url.rsplit('/', 3)[0]
-
         
         # 데이터 물리 저장 테이블 정보
         # 논리 테이블 영어
         table_logical_name = table_info_list.split('/')[0]
         table_logical_name = re.sub(r'(?<!^)(?=[A-Z])', '_', table_logical_name).upper()
 
-        # 일련번호, 데이터 기본 일련번호, 논리 테이블 한글, 논리 테이블 영어, 물리 테이블명
-        # 데이터 생성 유무(N), 테이블 생성일, 데이터 입력 유무(N)
-        
         table_physical_name = "REAL_" + str(data_basic_id).rjust(6, "0")
 
         ts = time.time()
@@ -103,11 +105,54 @@ if __name__=="__main__":
 
         insert_table_sql = """INSERT INTO MANAGE_PHYSICAL_TABLE (ID, DATA_BASIC_ID, LOGICAL_TABLE_HANGULE, LOGICAL_TABLE_ENGLISH, """\
                            """PHYSICAL_TABLE_NAME, PYHSICAL_CREATED_YN, TABLE_CREATE_DATE, DATA_INSERTED_YN) """\
-                           """VALUES (?, ?, ?, ?, ?, ?, ?, ?)"""
+                           """VALUES (SEQ_MTABLE.NEXTVAL, ?, ?, ?, ?, ?, ?, ?)"""
         
+        # SEQ_MTABLE.NEXTVAL, 데이터 기본 일련번호, 논리 테이블 한글, 논리 테이블 영어, 물리 테이블명
+        # 데이터 생성 유무(N), 테이블 생성일, 데이터 입력 유무(N)
+
         # 데이터 물리 저장 테이블 정보 INSERT
-        insert_table_values = (data_basic_id, data_basic_id, row['data_name'], table_logical_name, table_physical_name, 'N', timestamp, 'N')
+        insert_table_values = (data_basic_id, row['data_name'], table_logical_name, table_physical_name, 'N', timestamp, 'N')
         cur.execute(insert_table_sql, insert_table_values)
+
+        # TO-DO >> DATA SELECT 필요
+        
+        select_table_sql = """SELECT ID FROM MANAGE_PHYSICAL_TABLE WHERE DATA_BASIC_ID = {} """.format(data_basic_id)
+        
+        cur.execute(select_table_sql)
+        manage_table_result = cur.fetchall()
+        manage_table_id = manage_table_result[0][0]
+        
+
+        # 테이블 생성
+        create_table_sql = "CREATE TABLE " + table_physical_name + " (ID NUMBER, "
+
+        temp_sql = ""
+        for i in range(1, 101):
+            if i <= 50:
+                col_name = "COL_" + str(i).rjust(3, "0")
+                temp_sql += col_name + " VARCHAR(2000), "
+            else: 
+                col_name = "COL_" + str(i).rjust(3, "0")
+                temp_sql += col_name + " NUMBER(38)"
+
+                if i == 100:
+                    temp_sql += ") "
+                else:
+                    temp_sql += ", "
+
+        create_table_sql = create_table_sql + temp_sql        
+        print(create_table_sql)
+        
+        # 테이블 생성 쿼리 실행
+        cur.execute(create_table_sql)
+
+        # TO-DO >> MANGAE_TABLE_INFO 업데이트 진행 필요!!!
+
+
+
+
+
+
 
         # TO-DO >> 데이터 기본 정보 업데이트 진행 필요!!!
         
@@ -124,31 +169,32 @@ if __name__=="__main__":
                     logical_column_hangule = data_output_list[idx+2].text
                     logical_column_english = data_output_list[idx+1].text
                     physical_column_order = int(data_output_list[idx].text)
-                    physical_column_name = "COL_" + str(physical_column_order).rjust(4, "0")
+                    physical_column_name = "COL_" + str(physical_column_order).rjust(3, "0")
                    
                     ts_column = time.time()
                     timestamp_column = datetime.datetime.fromtimestamp(ts_column).strftime('%Y-%m-%d %H:%M:%S')
 
-                    insert_column_sql = """INSERT INTO MANAGE_PHYSICAL_COLUMN (ID, DATA_PYSICAL_ID, LOGICAL_COLUMN_HANGULE, LOGICAL_COLUMN_ENGLISH, """\
-                                        """PHYSICAL_COLUMN_ORDER, PHYSICAL_COLUMN_NAME, IS_CREATED_YN, COLUMN_CREATE_DATE) """\
-                                        """VALUES (?, ?, ?, ?, ?, ?, ?, ?)"""
-                    insert_column_values = (column_idx, data_basic_id, logical_column_hangule, logical_column_english,
-                                            physical_column_order, physical_column_name, 'N', timestamp_column)
-
+                    insert_column_sql = """INSERT INTO MANAGE_PHYSICAL_COLUMN (ID, DATA_PHYSICAL_ID, LOGICAL_COLUMN_HANGULE, LOGICAL_COLUMN_ENGLISH, """\
+                                        """PHYSICAL_COLUMN_ORDER, PHYSICAL_COLUMN_NAME, IS_USE_YN, IS_CREATED_YN, COLUMN_CREATE_DATE) """\
+                                        """VALUES (SEQ_MCOLUMN.NEXTVAL, ?, ?, ?, ?, ?, ?, ?, ?)"""
+                    
+                    
+                    insert_column_values = (manage_table_id, logical_column_hangule, logical_column_english,
+                                            physical_column_order, physical_column_name, 'Y', 'N', timestamp_column)
+                    
                     real_data_column += physical_column_name + ", "
-
 
                     cur.execute(insert_column_sql, insert_column_values)
 
                     column_idx += 1
 
         time.sleep(2)
+
         # DATA INSERT 컬럼 모음
         real_data_column = real_data_column.strip()
         real_data_column = real_data_column[:-1]
         real_data_column = real_data_column + ")"
        
-
         # OpenAPI 
         print("-------------------------------------------------------------------")
         print(sample_openapi_url)
@@ -161,11 +207,9 @@ if __name__=="__main__":
         test_xml = ET.fromstring(test_string)
 
         # 1000개 까지 검색 가능
-
         list_total_count = 0
 
-        for child in test_xml:
-            
+        for child in test_xml:            
             if child.tag == 'list_total_count':
                 list_total_count = int(child.text)
 
@@ -177,33 +221,9 @@ if __name__=="__main__":
         openapi_search_num = (list_total_count // 1000) + 1
         
         save_xml_path = './data/seoul/'
-
-
-        # TO-DO >> 데이터 입력
-        # 테이블 생성
-        create_table_sql = "CREATE TABLE " + table_physical_name + " (ID NUMBER, "
-
-        temp_sql = ""
-
-        for i in range(1, 101):
-            if i <= 50:
-                col_name = "COL_" + str(i).rjust(4, "0")
-                temp_sql += col_name + " VARCHAR(2000), "
-            else: 
-                col_name = "COL_" + str(i).rjust(4, "0")
-                temp_sql += col_name + " NUMBER(38)"
-
-                if i == 100:
-                    temp_sql += ") "
-                else:
-                    temp_sql += ", "
-
-        create_table_sql = create_table_sql + temp_sql  
         
-        print(create_table_sql)
-
-        cur.execute(create_table_sql)
-
+        df_save_data = pd.DataFrame()
+        
         for i in range(0, openapi_search_num):
             
             time.sleep(2)
@@ -226,48 +246,73 @@ if __name__=="__main__":
             search_openapi_content = search_openapi_response.content
 
             save_content = search_openapi_content.decode('utf-8')
-            save_xml = ET.fromstring(save_content)
 
-            save_file_name = table_physical_name + '_' + str(i) + '.xml'
+            error_check = 0
 
-            # TO-DO : DATA INSERT SQL 쿼리
-            # real_data_column
+            # 예외 처리 (OpenAPI 리턴 결과가 이상한 경우 제외)
+            try:
+                save_xml = ET.fromstring(save_content)
+                error_check = 1
+            except:
+                error_check = -1
 
-            # 1000개 까지 검색 가능
+            print("error_check = " + str(error_check))
 
-            temp_id = start_index
+            if error_check == 1:
+                # 에러가 아닐 경우에만 데이터 입력 진행
+                save_file_name = table_physical_name + '_' + str(i) + '.xml'
 
-            for child in save_xml:
-            
-                if child.tag == 'row':
-                    
-                    insert_column_num = len(child)
+                # TO-DO : DATA INSERT SQL 쿼리
+                # real_data_column
 
-                    insert_data_values = [temp_id]
+                # 1000개 까지 검색 가능
 
-                    for i in range(0, insert_column_num):
-                        insert_data_values.append(child[i].text)
+                temp_id = start_index
 
-                    insert_data_values = tuple(insert_data_values)
+                for child in save_xml:
+                
+                    if child.tag == 'row':
+                        
+                        insert_column_num = len(child)
+
+                        insert_data_values = [temp_id]
+
+                        for i in range(0, insert_column_num):
+
+                            temp_text = child[i].text
+
+                            if temp_text == None:
+                                temp_text = NULL
+                            else:
+                                temp_text = temp_text.replace("\n", " ")
+                                temp_text = temp_text.replace("\r", " ")
+                                
+                                if len(temp_text.encode('utf-8')) > 1990:
+                                    temp_text = ""
+
+                            insert_data_values.append(temp_text)
+
+                        insert_data_values = tuple(insert_data_values)
 
 
-                    temp_id += 1
-                    
+                        temp_id += 1
+                        
+                        df_save_data = df_save_data.append(pd.Series(insert_data_values), ignore_index=True)
+                        
+                        insert_data_sql = "INSERT INTO " + table_physical_name + " " + real_data_column + " VALUES " + str(insert_data_values)
+                        # print(insert_data_sql)
+                        cur.execute(insert_data_sql)
 
-                    insert_data_sql = "INSERT INTO " + table_physical_name + " " + real_data_column + " VALUES " + str(insert_data_values)
+                # 파일 저장
+                file_xml = open(save_xml_path + save_file_name, 'w', encoding="UTF-8")
+                file_xml.write(save_content)
+                file_xml.close()
+                time.sleep(3)
 
-                    cur.execute(insert_data_sql)
 
-            # 파일 저장
-            file_xml = open(save_xml_path + save_file_name, 'w', encoding="UTF-8")
-            file_xml.write(save_content)
-            file_xml.close()
-            time.sleep(3)
-            
-        
+        print("-----------------------------------------------------------------------------------------")    
 
         print("-----------------------------------------------------------------------------------------")
-        print("-----------------------------------------------------------------------------------------")
 
 
 
@@ -285,113 +330,5 @@ if __name__=="__main__":
 
     
 
-    print('=========================================================================================')
-    
-
-    
-
-    param_list = []
-
-    # 서울 데이터 URL
-    basic_url = "http://data.seoul.go.kr/dataList/"
-    search_url = basic_url + "datasetList.do"
-
-    # post 데이터
-    post_data = {"pageIndex1": "1", "sortColBy1": "R"}
-
-    response = requests.post(f'{search_url}', post_data)
-
-    detail_html = response.text
-    detail_soup = BeautifulSoup(detail_html, 'html.parser')
-
-    search_result = detail_soup.find("div", {"class": "search-result"})
-    search_result = int(search_result.find("strong").text.replace(",", ""))
-    
-    temp = search_result % 10
-    total_index = int(search_result / 10) + (0 if search_result % 10 == 0 else 1)
-
-    # 일부 데이터로 테스트
-    total_index = 3
-    
-    # 저장 데이셋
-    df_0_data = pd.DataFrame(columns=['title', 'data_rel', 'data_type', 'make_url'])
-
-
-    # 리스트에 있는 데이터셋 기본 항목 저장
-    for i in tqdm(range(0, total_index)):
-
-        save_index = i * 10
-
-        time.sleep(5)
-
-        post_index_data = {"pageIndex1": i+1, "sortColBy1": "R"}
-
-        response_index = requests.post(f'{search_url}', post_index_data)
-        html_index = response_index.text
-
-        soup_index = BeautifulSoup(html_index, 'html.parser')
-        dl_list = soup_index.find_all("dl", {"class": "type-b"})
-
-        for dl_one in dl_list:
-            
-            chk_openapi = [0, 0]            # OpenAPI 인 경우
-
-            # Insert Data 정의
-            save_index += 1
-
-            ts = time.time()
-            timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-
-            # Title 정보
-            alinks = dl_one.find_all("a", {"class": "goView"})
-
-            title_name = alinks[0].text.strip()
-            data_rel = alinks[0].attrs['data-rel'].split("/")[0]
-            
-            provide_list_data_type = []
-            provide_list_url = []
-
-            collect_data_type = ""
-            collect_url = ""
-
-            # Data 타입 정보
-            buttons = dl_one.find_all("button")
-
-            for i in range(len(buttons)):
-                
-                button_text = buttons[i].text.strip()
-                
-                provide_list_data_type.append(button_text)
-                provide_list_url.append(buttons[i].attrs['data-rel'])
-
-                if button_text == 'OpenAPI':
-                    chk_openapi[0] = 1      # OpenAPI 인 경우 1
-                    chk_openapi[1] = i      # OpenAPI 리스트에서의 위치
-
-            if chk_openapi[0] == 1:
-                # Open API 인 경우
-                print(chk_openapi)
-                collect_data_type = 'OpenAPI'
-                collect_url = basic_url + provide_list_url[chk_openapi[1]]
-            else:
-                # Open API 가 아닌 경우에는 0번째 타입으로 정의
-                collect_data_type = provide_list_data_type[0]
-                collect_url = basic_url + provide_list_url[0]
-                
-                print(chk_openapi)
-            
-
-            insert_sql = """INSERT INTO DATA_BASIC_INFO (ID, COLLECT_SITE_ID, DATA_NAME, PROVIDE_DATA_TYPE, PROVIDE_URL_LINK, """\
-                         """COLLECT_DATA_TYPE, COLLECT_URL_LINK, IS_COLLECT_YN, SYSTEM_REGISTER_DATE) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"""
-
-            insert_table_values = (save_index, 1, title_name, str(provide_list_data_type), str(provide_list_url), collect_data_type, collect_url, 'N', timestamp)
-            cur.execute(insert_sql, insert_table_values)
-            
-            df_0_data = df_0_data.append({'title': title_name, 'data_rel': data_rel, 'data_type': provide_list_data_type, 'make_url': provide_list_url}, ignore_index=True)
-
-
-
-
-    cur.close()
-
+    print('=========================================================================================')    
     print('=========================================================================================')

604346e036fb92e1e476bedfb900c994b41e24ac
DongHyeonKim-tech <sxiloveixz00@nate.com>
2022-08-01 14:16:04 +0900

    [공공수정] 파일명 및 주석 수정

--- 1_1_data_public_download_csv.py
+++ 1_1_data_public_download_csv.py
@@ -43,9 +43,9 @@ if __name__ == "__main__":
                 # 암묵적 대기 5s
                 driver.implicitly_wait(5)
                 try:
-                    # CSV 파일 다운로드
+                    # 1.CSV 파일 다운로드
                     driver.find_element_by_xpath(f'//*[@id="fileDataList"]/div[2]/ul/li[{i}]/div[2]/a').send_keys(Keys.ENTER)
-                    # DATA_BASIC_INFO 테이블 필요 데이터 search
+                    # 2.DATA_BASIC_INFO 테이블 필요 데이터 search
                     category = driver.find_element_by_xpath(f'//*[@id="fileDataList"]/div[2]/ul/li[{i}]/p/span[1]').text
                     desc = driver.find_element_by_xpath(f'//*[@id="fileDataList"]/div[2]/ul/li[{i}]/dl/dd').text.replace("'","")
                     name = driver.find_element_by_xpath(f'//*[@id="fileDataList"]/div[2]/ul/li[{i}]/dl/dt/a/span[@class="title"]').text
@@ -55,7 +55,7 @@ if __name__ == "__main__":
                 except NoSuchElementException:
                     continue
                 
-                # 다운로드한 데이터의 data_origin_key를 기존 DATA_BASIC_INFO 테이블 내 데이터와 비교
+                # 3.다운로드한 데이터의 data_origin_key를 기존 DATA_BASIC_INFO 테이블 내 데이터와 비교
                 inserted_check_sql = f"select id from DATA_BASIC_INFO where data_origin_key='{key}'" 
                 cur.execute(inserted_check_sql)
                 inserted_check = cur.fetchall()
@@ -66,7 +66,7 @@ if __name__ == "__main__":
                     cur.execute(sql)
                     print('inserted')
                     
-                    # 다운로드, insert가 완료된 파일을 1_1_data_public_read_csv.py에 사용 가능하도록 이동
+                    # 4.다운로드, insert가 완료된 파일을 1_1_data_public_read_csv.py에 사용 가능하도록 이동
                     time.sleep(3)
                     file_list = os.listdir(current_file_path)
                     for file in file_list:
--- 1_2_data_public_read_csv.py
+++ 1_2_data_public_read_csv.py
@@ -20,7 +20,7 @@ if __name__ =="__main__":
     
     destination_path = 'C:/euclid/nl2sql/done'
 
-    ## CSV 파일 읽기
+    ## 1.CSV 파일 읽기
     for file in file_list:
         try:
             f = open(path_dir+ '/' + file, 'r', encoding='cp949')
@@ -40,11 +40,7 @@ if __name__ =="__main__":
         except UnicodeDecodeError as e:
             continue
         
-        ## MANAGE_PHYSICAL_TABLE 테이블 데이터 insert    
-        table_max_id_sql = "select max(id) from MANAGE_PHYSICAL_TABLE"
-        cur.execute(table_max_id_sql)
-        table_max_id = cur.fetchone()[0] + 1
-        
+
         print("######################################")
         print(f'file name: {name}')
         data_basic_sql = f"select id from DATA_BASIC_INFO where DATA_NAME='{name}' and collect_sitd_id='2' and is_collect_yn='N'"
@@ -55,6 +51,11 @@ if __name__ =="__main__":
         if data_basic_fetch and extn == 'csv':
             print("-----------------------------------")
             print('start insert')
+            ## 2.MANAGE_PHYSICAL_TABLE 테이블 데이터 insert    
+            table_max_id_sql = "select max(id) from MANAGE_PHYSICAL_TABLE"
+            cur.execute(table_max_id_sql)
+            table_max_id = cur.fetchone()[0] + 1
+            
             data_basic_id = data_basic_fetch[0]
             physical_table_name = 'NLDATA_' + str(data_basic_id).rjust(6, '0')
             orig_table_name = 'TMP100_' + str(data_basic_id)
@@ -62,7 +63,7 @@ if __name__ =="__main__":
             TABLE_SQL = f"insert into MANAGE_PHYSICAL_TABLE(ID,DATA_BASIC_ID, LOGICAL_TABLE_KOREAN, PHYSICAL_CREATED_YN, DATA_INSERTED_YN, DATA_INSERT_ROW, TARGET_ROWS, physical_table_name, orig_table_name, logical_table_english) VALUES('{table_max_id}', '{data_basic_id}', '{name}', 'N', 'N', '0', '{len_rows}', '{physical_table_name}', '{orig_table_name}', '{logical_table_english}')"
             cur.execute(TABLE_SQL)
             
-            ## MANAGE_PHYSICAL_COLUMN 테이블 데이터 insert
+            ## 3.MANAGE_PHYSICAL_COLUMN 테이블 데이터 insert
             print("-----------------------------------")
             print('MANAGE_PHYSICAL_TABLE inserted')
             column = data_list[0]
@@ -85,18 +86,8 @@ if __name__ =="__main__":
                 physical_column_order += 1
             print("-----------------------------------")
             print('MANAGE_PHYSICAL_COLUMN inserted')
-        
-        ## MANAGE_PHYSICAL_TABLE physical_created_yn N -> Y update
-            update_table_physical_created_yn_sql = f"update MANAGE_PHYSICAL_TABLE set PHYSICAL_CREATED_YN='Y' where id='{table_max_id}'"
-            cur.execute(update_table_physical_created_yn_sql)        
-            print("-----------------------------------")
-            print('MANAGE_PHYSICAL_TABLE physical_created_yn update')
-            
-            data = data_list[1:]
-            col_list = ["COL_" + str(i).rjust(3, '0') + " VARCHAR(65532), " if i<len(column) else "COL_" + str(i).rjust(3, '0') + " VARCHAR(65532)" for i in range(1, len(column)+1)]
-            col_data = ''.join(col_list)
-            
-            ## TMP 테이블 create
+
+            ## 4.TMP 테이블 create
             # print("######################################")
             # print(f'table name: {orig_table_name}')
             # create_tmp_sql = f"create table {orig_table_name} (ID NUMBER, {col_data})"
@@ -104,15 +95,19 @@ if __name__ =="__main__":
             # print("-----------------------------------")
             # print('TMP table created')
             
-            ## NLDATA 테이블 create
+            ## 5.NLDATA 테이블 create
             print(f'table name: {physical_table_name}')
+            data = data_list[1:]
+            col_list = ["COL_" + str(i).rjust(3, '0') + " VARCHAR(65532), " if i<len(column) else "COL_" + str(i).rjust(3, '0') + " VARCHAR(65532)" for i in range(1, len(column)+1)]
+            col_data = ''.join(col_list)
+                        
             create_nl_sql = f"create table {physical_table_name} (ID NUMBER, {col_data})"
             cur.execute(create_nl_sql)
             print("-----------------------------------")
             print('NLDATA table created')
 
             
-            ## TMP 테이블에 데이터 insert
+            ## 6.NLDATA 테이블에 데이터 insert
             id = 1
             if len_rows:
                 for dt in data:
@@ -127,13 +122,15 @@ if __name__ =="__main__":
                 # print('TMP_DATA inserted')
                 print('NLDATA inserted')
 
-                
+                ## 7. UPDATE
                 ## DATA_BASIC_INFO 테이블 is_collect_yn N -> Y update
                 update_basic_sql = f"update DATA_BASIC_INFO set is_collect_yn='Y' where id='{data_basic_id}'"
                 cur.execute(update_basic_sql)
                 ## MANAGE_PHYSICAL_TABLE 테이블 data_inserted_yn N -> Y, data_insert_row None -> len_rows update 
                 update_table_sql = f"update MANAGE_PHYSICAL_TABLE set data_inserted_yn='Y', physical_created_yn='Y', data_insert_row={len_rows} where id='{table_max_id}'"
                 cur.execute(update_table_sql)
+                ## MANAGE_PHYSICAL_COLUMN 테이블 is_created_yn N -> Y
+                update_column_sql = f"update MANAGE_PHYSICAL_COLUMN set is_created_yn='Y' where data_physical_id={table_max_id}"
                 print("-----------------------------------")
                 print('table updated')
         shutil.move(f'{path_dir}/{file}', destination_path)
